657d34682d777532398813ed478fb80015c695e2
ZaibLComrade <kmjahanzaib@gmail.com>
2023-12-13 07:28:08 +0600

    Dropdowns now close on outside when active

--- src/layout/shared/Navbar.jsx
+++ src/layout/shared/Navbar.jsx
@@ -16,7 +16,7 @@ export default function Navbar() {
 		<li><NavLink to="/donation/campaigns">Donation Campaign</NavLink></li>
 	</>
 	return <div className="z-[9999]">
-		<div className="relative flex items-center justify-between h-20 px-8 md:h-24 bg-secondary-2">
+		<div className="relative flex items-center justify-between h-16 px-8 md:h-20 bg-secondary-2">
 		{/* Menu Icon displayed in small screen */}
 			<div className="md:hidden">
 				<button onClick={ () => {setOpenMenu(!openMenu); setOpenAvatar(false)} }>
@@ -24,16 +24,19 @@ export default function Navbar() {
 			</button> 
 		</div>
 		
+		{/* Overlay to close dropdowns */}
+			<div onClick={ () => { setOpenMenu(false); setOpenAvatar(false) } } className={`fixed ${(openMenu || openAvatar) ? "block" : "hidden"} top-0 left-0 w-screen h-screen z-[50]`}></div>
+		
 		{/* Logo */}
 		<button>
 		<Link to="/" className="flex items-center lg:gap-4 gap-2">
 			<img className="hidden w-12 md:block lg:w-16" src="/logo.png" alt="Logo"/>
-			<h1 className="text-5xl lg:text-6xl text-primary font-banger">Purrlinks</h1>
+			<h1 className="text-4xl lg:text-6xl text-primary font-banger">Purrlinks</h1>
 		</Link>
 		</button>
 				
 		{/* Menu Dropdown */}
-		<div className={`z-50 ${openMenu ? "block md:hidden" : "hidden"} my-4 absolute top-14 left-2 text-base list-none bg-neutral rounded shadow divide-y divide-gray-100`} >
+		<div className={`z-50 ${openMenu ? "block md:hidden" : "hidden"} my-4 absolute top-12 left-2 text-base list-none bg-neutral rounded shadow divide-y divide-gray-100`} >
 			<ul className="py-1">
 				<li className={ dropdownListStyle }><NavLink to="/">Home</NavLink></li>
 				<li className={ dropdownListStyle }><NavLink to="/pets/available">Pet Listing</NavLink></li>
@@ -53,7 +56,7 @@ export default function Navbar() {
 		
 		{/* User */}
 		<div className="flex">
-			<button onClick={ () => {setOpenAvatar(!openAvatar); setOpenMenu(false)} } className="w-12 h-12 rounded-full outline outline-black/5 hover:outline-black/10 transition ease-out duration-200 hover:-translate-y-px md:h-12">
+			<button onClick={ () => {setOpenAvatar(!openAvatar); setOpenMenu(false)} } className="w-8 h-8 rounded-full md:w-12 md:h-12 outline outline-black/5 hover:outline-black/10 transition ease-out duration-200 hover:-translate-y-px">
 				<div className="w-full h-full">
 					<img 
 						src={ user?.photoURL ? user.photoURL : dummyUser } 
@@ -63,26 +66,26 @@ export default function Navbar() {
 			</button>
 		</div>
 			
-		{/* Avatar Dropdown */}
-		<div className={`z-50 ${openAvatar ? "block" : "hidden"} my-4 absolute top-16 right-2 text-base list-none bg-neutral rounded shadow divide-y divide-gray-100`} >
-			{ user ? <div className="px-4 py-3" role="none">
-				<p className="text-sm text-gray-900">{ user.displayName }</p>
-				<p className="text-sm font-medium text-gray-900 truncate">{ user.email }</p>
-			</div>
-			: <div className="px-4 py-3">
-				<p className="text-sm font-medium text-gray-900">Not Logged In</p>
-			</div>
-			}
-			<ul className="py-1 min-w-[130px]">
-				{user  
-					? <>
-						<li className={ dropdownListStyle }><NavLink to="/dashboard/adoption/my">Dashboard</NavLink></li>
-						<li className={ dropdownListStyle }><button onClick={ logOut }>Logout</button></li>
-					</>
-					: <li className={ dropdownListStyle }><NavLink to="/login">Login</NavLink></li>
+			{/* Avatar Dropdown */}
+			<div className={`z-50 ${openAvatar ? "block" : "hidden"} my-4 absolute top-12 md:top-16 right-2 text-base list-none bg-neutral rounded shadow divide-y divide-gray-100`} >
+				{ user ? <div className="px-4 py-3" role="none">
+					<p className="text-sm text-gray-900">{ user.displayName }</p>
+					<p className="text-sm font-medium text-gray-900 truncate">{ user.email }</p>
+				</div>
+				: <div className="px-4 py-3">
+					<p className="text-sm font-medium text-gray-900">Not Logged In</p>
+				</div>
 				}
-			</ul>
-		</div>
+				<ul className="py-1 min-w-[130px]">
+					{user  
+						? <>
+							<li className={ dropdownListStyle }><NavLink to="/dashboard/adoption/my">Dashboard</NavLink></li>
+							<li className={ dropdownListStyle }><button onClick={ logOut }>Logout</button></li>
+						</>
+						: <li className={ dropdownListStyle }><NavLink to="/login">Login</NavLink></li>
+					}
+				</ul>
+			</div>
 		</div>
 	</div>
 }
