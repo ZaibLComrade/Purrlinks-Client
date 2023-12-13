253cd79a6c369e1cf62dc5f246a42dffc41a8add
ZaibLComrade <kmjahanzaib@gmail.com>
2023-12-13 07:37:44 +0600

    Modified category dropdown

--- src/layout/home/category/Category.jsx
+++ src/layout/home/category/Category.jsx
@@ -81,11 +81,14 @@ export default function Category() {
 				}
 			</div>
 			
+			{/* Overlay to close dropdowns */}
+			<div onClick={ () => { setIsOpenMenu(false) } } className={`fixed ${isOpenMenu ? "block" : "hidden"} top-0 left-0 w-screen h-screen z-[50]`}></div>
+			
 			{/* Dropdown */}
 			{
-				isOpenMenu && <div className={`md:right-3 right-0 absolute lg:-bottom-14 z-[9999]`}>
-				<div className="border rounded-bl-lg border-secondary-2 md:rounded-b-lg w-max bg-neutral">
-					<ul className="py-2 px-8 h-[100px] overflow-y-scroll dropdown divide-y">
+				isOpenMenu && <div className={`absolute shadow-lg rounded-lg right-0 lg:top-14 lg:-bottom-14 z-[9999]`}>
+				<div className="border rounded-bl-lg border-secondary-2 w-max bg-neutral">
+					<ul className="px-8 py-2 overflow-y-scroll dropdown divide-y">
 						{
 							category.map(({ label, icon: Icon }, i) => {
 								if(i >= maxItemDisplay) return createDropdownItem(label, Icon)
